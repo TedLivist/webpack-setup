f1220ea998196ca1b6d8615455449358aab27ac1
Teddy-Livingstone Ememandu <livistteddy@gmail.com>
2021-08-16 22:59:54 +0100

    Build basic html file with HtmlWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: './src/index.js',
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Webpack Setup',
+      template: './src/index.html'
     }),
   ],
   output: {
