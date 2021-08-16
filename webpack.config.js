7c1daafb96d91b6cc8c6ad1c248c76455aead5ff
Teddy-Livingstone Ememandu <livistteddy@gmail.com>
2021-08-16 23:14:07 +0100

    Add webpack server

--- webpack.config.js
+++ webpack.config.js
@@ -8,9 +8,13 @@ module.exports = {
       template: './src/index.html'
     }),
   ],
+  devServer: {
+    contentBase: './dist',
+  },
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
   module: {
     rules: [
